60cc3cd9dee0fe7ee5d1b7b601f342f2cbad0730
Zahra Ghasempour <ghasempour.zahra0@gmail.com>
2025-02-28 13:43:08 +0200

    add git ignore

---
+++ src/main/java/com/first/spring/boot/project/create_RESTful_API/CreateResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.first.spring.boot.project.create_RESTful_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/spring/boot/project/create_RESTful_API/CreateResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.spring.boot.project.create_RESTful_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
